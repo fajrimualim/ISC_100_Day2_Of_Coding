a323e44cdb14d27c143fc5b3e09c3bb8e540ab2e
fajrimualim <147229764+fajrimualim@users.noreply.github.com>
2023-10-10 04:44:29 -0700

    Add files via upload

---
+++ Hari2.java
@@ -0,0 +1,28 @@
+
+package Fajrin;
+
+
+public class Hari2 {
+    public static void main(String[] args) {
+        int integerData = 42;
+        double doubleData = 3.14159;
+        char charData = 'A';
+        boolean booleanData = true;
+        byte byteData = 127;
+        short shortData = 32767;
+        long longData = 1234567890L;
+        float floatData = 1.23f;
+        
+        System.out.println("Data Integer: " + integerData);
+        System.out.println("Data Double: " + doubleData);
+        System.out.println("Data Char: " + charData);
+        System.out.println("Data Boolean: " + booleanData);
+        System.out.println("Data Byte: " + byteData);
+        System.out.println("Data Short: " + shortData);
+        System.out.println("Data Long: " + longData);
+        System.out.println("Data Float: " + floatData);
+    }
+    
+}
+
+    
